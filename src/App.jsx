c40152af9df20cc799e161b974ed3b2de8d10627
Gungun-Pathak <gungunpathak600@gmail.com>
2023-10-20 15:41:35 +0530

    index page added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import { useState } from 'react'
-import Navbar from './components/Navbar';
-import Sidebar from './components/Sidebar';
 import './App.css'
+import Index from '.';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-<Navbar />
+<Index/>
 {/* <Sidebar /> */}
     </>
   )
